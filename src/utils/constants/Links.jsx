308b5a33393f48b840967b356778dc9346a3b2d3
“milliybro” <shohrux-rustamov@mail.ru>
2024-07-08 04:22:36 +0500

    second

--- src/utils/constants/Links.jsx
+++ src/utils/constants/Links.jsx
@@ -1,7 +1,7 @@
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 
 export const LinksData = () => {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
 
   return [
     {
